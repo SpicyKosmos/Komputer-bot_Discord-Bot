957e3e69ffaf6d14c58e9a08bc4b6245b444fb1d
Kaan Zati <kaan.zati@gmail.com>
2023-05-13 17:39:08 +0300

    update

--- Komputer.py
+++ Komputer.py
@@ -9,8 +9,8 @@ import requests
 
 
 
-TOKEN= "MTA5ODk2NjcwMzgxNzM3OTkwMA.GTi3yd.TAWOZHLKzXoHNxs0DdZQCJnki_QleVlU1OJy0Q"
-sunucu_id = "263045299755417630"
+TOKEN= #Your Discord Token
+sunucu_id = #Your Server ID
 channel1 = "bot-test"
 
 
